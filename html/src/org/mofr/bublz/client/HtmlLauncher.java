ea58bb46cf35e41fcc7d71ce6932031c5d131d2c
mofr <mofr@zond.org>
2019-02-21 23:42:40 +0700

    Fix indents

--- html/src/org/mofr/bublz/client/HtmlLauncher.java
+++ html/src/org/mofr/bublz/client/HtmlLauncher.java
@@ -11,35 +11,35 @@ import org.mofr.bublz.Game;
 
 public class HtmlLauncher extends GwtApplication {
 
-         private static final int PADDING = 0;
-         private GwtApplicationConfiguration cfg;
+    private static final int PADDING = 0;
+    private GwtApplicationConfiguration cfg;
 
-         @Override
-         public GwtApplicationConfiguration getConfig() {
-             int w = Window.getClientWidth() - PADDING;
-             int h = Window.getClientHeight() - PADDING;
-             cfg = new GwtApplicationConfiguration(w, h);
-             Window.enableScrolling(false);
-             Window.setMargin("0");
-             Window.addResizeHandler(new ResizeListener());
-             cfg.preferFlash = false;
-             return cfg;
-         }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        int w = Window.getClientWidth() - PADDING;
+        int h = Window.getClientHeight() - PADDING;
+        cfg = new GwtApplicationConfiguration(w, h);
+        Window.enableScrolling(false);
+        Window.setMargin("0");
+        Window.addResizeHandler(new ResizeListener());
+        cfg.preferFlash = false;
+        return cfg;
+    }
 
-         class ResizeListener implements ResizeHandler {
-             @Override
-             public void onResize(ResizeEvent event) {
-                 int width = event.getWidth() - PADDING;
-                 int height = event.getHeight() - PADDING;
-                 getRootPanel().setWidth("" + width + "px");
-                 getRootPanel().setHeight("" + height + "px");
-                 getApplicationListener().resize(width, height);
-                 Gdx.graphics.setWindowedMode(width, height);
-             }
-         }
-
+    class ResizeListener implements ResizeHandler {
         @Override
-        public ApplicationListener createApplicationListener () {
-                return new Game();
+        public void onResize(ResizeEvent event) {
+            int width = event.getWidth() - PADDING;
+            int height = event.getHeight() - PADDING;
+            getRootPanel().setWidth("" + width + "px");
+            getRootPanel().setHeight("" + height + "px");
+            getApplicationListener().resize(width, height);
+            Gdx.graphics.setWindowedMode(width, height);
         }
+    }
+
+    @Override
+    public ApplicationListener createApplicationListener () {
+        return new Game();
+    }
 }
